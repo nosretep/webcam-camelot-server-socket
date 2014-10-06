e624821c070b36436b779d16e9df1eb120a5422f
Mark Peterson <playerspeak@gmail.com>
2014-10-05 23:28:13 -0400

    Delete leaf.js, tree.js.

--- server.js
+++ server.js
@@ -1,45 +1,65 @@
+var requirejs = require('requirejs');
 
-var app = require('express')();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-var camelot = require('camelot');
-
-server.listen(8888);
-
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
+requirejs.config({
+    nodeRequire: require
 });
 
-io.sockets.on('connection', function(socket){
-  camera.on('frame', function(imagedata){
-	  var image64 = imagedata.toString('base64');
-	  socket.emit('frame', image64);
-  });
-});
+requirejs([
+    'http', 
+    'express',
+    'socket.io',
+    'camelot'], 
 
-var camera = new camelot({
-  'palette': 'YUYV',
-  'device': '/dev/video0',
-  'jpeg': '85',
-  'resolution': '320x240',
-//  'greyscale' : true,
-  'controls' : {
-    focus : 'auto',
-    brightness : 225,
-    contrast : 125,
-    saturation : 125,
-    hue : 125,
-    gamma : 125,
-    sharpness : 125
-  }
-});
+function(
+    http,
+    express,
+    socketio,
+    camelot) {
 
-camera.on('error', function(error){
-  console.log(error);
-});
+	var app = express();
+	var server = http.createServer(app);
+	var io = socketio.listen(server);
+		io.set('log level', 0);
 
-camera.grab({
-  'title': 'Camera0',
-  'font': 'Arial:12',
-  'frequency': 1
+	server.listen(8888);
+
+	app.get('/', function(req, res){
+	  res.sendfile(__dirname + '/index.html');
+	});
+	
+	io.sockets.on('connection', function(socket){
+	  camera.on('frame', function(imagedata){
+		  var image64 = imagedata.toString('base64');
+		  socket.emit('frame', image64);
+	  });
+	});
+	
+	var camera = new camelot({
+	  'palette': 'YUYV',
+	  'device': '/dev/video0',
+	  'jpeg': '85',
+	  'resolution': '320x240'
+//		  ,
+	//  'greyscale' : true,
+//	  'controls' : {
+//	    focus : 'auto',
+//	    brightness : 225,
+//	    contrast : 125,
+//	    saturation : 125,
+//	    hue : 125,
+//	    gamma : 125,
+//	    sharpness : 125
+//	  }
+	});
+	
+	camera.on('error', function(error){
+	  console.log(error);
+	});
+	
+	camera.grab({
+	  'title': 'Camera0',
+	  'font': 'Arial:12',
+	  'frequency': 1
+	});
+
 });
