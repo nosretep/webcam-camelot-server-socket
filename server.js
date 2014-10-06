0477ae0f5d41269fb3bb33e5cd4190ed3fbdc50e
Mark Peterson <playerspeak@gmail.com>
2014-10-06 00:04:11 -0400

    Fixed spacing

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 var requirejs = require('requirejs');
 
 requirejs.config({
-    nodeRequire: require
+	nodeRequire : require
 });
 
-requirejs([
-    'http', 
-    'express',
-    'socket.io',
-    'camelot'], 
+requirejs([ 'http', 'express', 'socket.io', 'camelot' ],
 
-function(
-    http,
-    express,
-    socketio,
-    camelot) {
+function(http, express, socketio, camelot) {
 
-    var app = express();
-    var server = http.createServer(app);
-    var io = socketio.listen(server);
-        io.set('log level', 0);
+	var app = express();
+	var server = http.createServer(app);
+	var io = socketio.listen(server);
+	io.set('log level', 0);
 
-    server.listen(8888);
+	server.listen(8888);
 
-    app.get('/', function(req, res) {
+	app.get('/', function(req, res) {
 		res.sendfile(__dirname + '/index.html');
 	});
 
@@ -33,29 +25,29 @@ function(
 			socket.emit('frame', image64);
 		});
 	});
-    
-    var camera = new camelot({
-      'palette': 'YUYV',
-      'device': '/dev/video0',
-      'jpeg': '85',
-      'resolution': '320x240'
-//          ,
-    //  'greyscale' : true,
-//      'controls' : {
-//        focus : 'auto',
-//        brightness : 225,
-//        contrast : 125,
-//        saturation : 125,
-//        hue : 125,
-//        gamma : 125,
-//        sharpness : 125
-//      }
-    });
+
+	var camera = new camelot({
+		'palette' : 'YUYV',
+		'device' : '/dev/video0',
+		'jpeg' : '85',
+		'resolution' : '320x240'
+	//          ,
+	//  'greyscale' : true,
+	//      'controls' : {
+	//        focus : 'auto',
+	//        brightness : 225,
+	//        contrast : 125,
+	//        saturation : 125,
+	//        hue : 125,
+	//        gamma : 125,
+	//        sharpness : 125
+	//      }
+	});
 
 	camera.on('error', function(error) {
 		console.log(error);
 	});
-	
+
 	camera.grab({
 		'title' : 'Camera0',
 		'font' : 'Arial:12',
