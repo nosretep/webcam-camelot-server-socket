2bb02795551ce709bc36d74c7d32daee6ad692a6
Mark Peterson <playerspeak@gmail.com>
2014-10-06 11:20:42 -0400

    Spaces instead of tabs

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 var requirejs = require('requirejs');
 
 requirejs.config({
-	nodeRequire : require
+    nodeRequire : require
 });
 
 requirejs([ 'http', 'express', 'socket.io', 'camelot' ],
 
 function(http, express, socketio, camelot) {
 
-	var clients = 0;
-	var app = express();
-	var server = http.createServer(app);
-	var io = socketio.listen(server);
-		io.set('log level', 0);
-		
+    var clients = 0;
+    var app = express();
+    var server = http.createServer(app);
+    var io = socketio.listen(server);
+
+    io.set('log level', 0);
     var pic = io.of('/picture');
-	    pic.on('connection', function(socket) {});
+        pic.on('connection', function(socket) {
+    });
 
-	server.listen(8888);
+    server.listen(8888);
 
-	app.get('/', function(req, res) {
-		res.sendfile(__dirname + '/index.html');
-	});
+    app.get('/', function(req, res) {
+        res.sendfile(__dirname + '/index.html');
+    });
 
-	io.sockets.on('connection', function(socket) {
+    io.sockets.on('connection', function(socket) {
         var address = socket.handshake.address;
         console.log("New connection from " + address.address + ":" + address.port);
         clients++;
@@ -32,39 +33,39 @@ function(http, express, socketio, camelot) {
             console.log("Client disconnected " + address.address + ":" + address.port);
             clients--;
         });
-	});
-	
-	var camera = new camelot({
-		'palette' : 'YUYV',
-		'device' : '/dev/video0',
-		'jpeg' : '85',
-		'resolution' : '320x240'
-	//          ,
-	//  'greyscale' : true,
-	//      'controls' : {
-	//        focus : 'auto',
-	//        brightness : 225,
-	//        contrast : 125,
-	//        saturation : 125,
-	//        hue : 125,
-	//        gamma : 125,
-	//        sharpness : 125
-	//      }
-	});
+    });
 
-	camera.on('frame', function(imagedata) {
-		var image64 = imagedata.toString('base64');
+    var camera = new camelot({
+        'palette' : 'YUYV',
+        'device' : '/dev/video0',
+        'jpeg' : '85',
+        'resolution' : '320x240'
+    // ,
+    // 'greyscale' : true,
+    // 'controls' : {
+    // focus : 'auto',
+    // brightness : 225,
+    // contrast : 125,
+    // saturation : 125,
+    // hue : 125,
+    // gamma : 125,
+    // sharpness : 125
+    // }
+    });
+
+    camera.on('frame', function(imagedata) {
+        var image64 = imagedata.toString('base64');
         pic.volatile.emit('frame', image64);
-	});
-	
-	camera.on('error', function(error) {
-		console.log(error);
-	});
+    });
 
-	camera.grab({
-		'title' : 'Camera0',
-		'font' : 'Arial:12',
-		'frequency' : 1
-	});
+    camera.on('error', function(error) {
+        console.log(error);
+    });
+
+    camera.grab({
+        'title' : 'Camera0',
+        'font' : 'Arial:12',
+        'frequency' : 1
+    });
 
 });
